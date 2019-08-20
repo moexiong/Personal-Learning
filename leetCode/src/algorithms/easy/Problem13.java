4cee5e5200d4159143fb0cfe1d93fe6ec492d563
moexiong <zsx_alan@qq.com>
2019-08-21 00:31:49 +0800

    leetcode-easy:13.罗马数字转整数 附最优速度解答

---
+++ leetCode/src/algorithms/easy/Problem13.java
@@ -0,0 +1,158 @@
+package algorithms.easy;
+
+/**
+ * 罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+ *
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ *
+ * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+ *
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+ *
+ * I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+ * X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+ * C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+ * 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
+ *
+ * 示例 1:
+ *
+ * 输入: "III"
+ * 输出: 3
+ * 示例 2:
+ *
+ * 输入: "IV"
+ * 输出: 4
+ * 示例 3:
+ *
+ * 输入: "IX"
+ * 输出: 9
+ * 示例 4:
+ *
+ * 输入: "LVIII"
+ * 输出: 58
+ * 解释: L = 50, V= 5, III = 3.
+ * 示例 5:
+ *
+ * 输入: "MCMXCIV"
+ * 输出: 1994
+ * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+ *
+ */
+public class Problem13 {
+
+    public static int romanToInt(String s) {
+        if (s == null || "".equals(s)) {
+            return 0;
+        }
+        return doRoman(s.toUpperCase());
+    }
+
+    /**
+     * 对字符串进行解析
+     * @param s
+     * @return
+     */
+    private static int doRoman(String s) {
+        String[] types = new String[]{"M", "D", "C", "L", "X", "V", "I"};
+        int number = 0;
+        for (int i = 0; i < types.length; i++) {
+            if (s.contains(types[i])) {
+                String temp = s.substring(0, s.indexOf(types[i]) + 1);
+                number += getPartValue(temp);
+                s = s.substring(s.indexOf(types[i]) + 1);
+                i--;
+            }
+        }
+        return number;
+    }
+
+    /**
+     * 对每一段进行解析
+     * @param seq
+     * @return
+     */
+    private static int getPartValue(String seq) {
+        char[] ss = seq.toCharArray();
+        if (ss.length == 1) {
+            return getValue(ss[0]);
+        }
+        int num = getValue(ss[ss.length - 1]);
+        for (int i = ss.length - 2; i >= 0; i--) {
+            num -= getValue(ss[i]);
+        }
+        return num;
+    }
+
+    /**
+     * 获取对应字母表示的数值
+     * @param c
+     * @return
+     */
+    private static int getValue(char c) {
+        switch (c) {
+            case 'M' :
+                return 1000;
+            case 'D' :
+                return 500;
+            case 'C' :
+                return 100;
+            case 'L' :
+                return 50;
+            case 'X' :
+                return 10;
+            case 'V' :
+                return 5;
+            case 'I' :
+                return 1;
+        }
+        return 0;
+    }
+
+    /**
+     * 最优解答
+     * @param s
+     * @return
+     */
+    public int romanToInt_best(String s) {
+        int result = 0;
+
+        for (int i = s.length()-1, rLast = 0; i >= 0; i--) {
+            char c = s.charAt(i);
+            int r = romanCharToInt(c);
+            if (r < rLast) {
+                result -= r;
+            } else {
+                result += r;
+            }
+            rLast = r;
+        }
+        return result;
+    }
+
+    private int romanCharToInt(char c) {
+        int result = 0;
+        switch (c){
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("IX：" + romanToInt("IX"));
+        System.out.println("LVIII：" + romanToInt("LVIII"));
+        System.out.println("MCMXCIV：" + romanToInt("MCMXCIV"));
+    }
+}
